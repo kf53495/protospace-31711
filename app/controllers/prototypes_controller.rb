bd1dc743bf8dbf93bad4b4ba12a08c20ffceca1d
kf53495 <fkmndk902902@gmail.com>
2020-11-09 14:13:51 +0900

    ログイン中ではないユーザーでも一覧ページを見ることができるように修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index]
   def index 
     @prototypes = Prototype.includes(:user).order("created_at DESC")
   end
